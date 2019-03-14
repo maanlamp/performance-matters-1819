eab05c67d36bb540ca1b4dfb4c13a3b45b480fe0
maanlamp <32102935+maanlamp@users.noreply.github.com>
2019-03-14 20:29:23 +0100

    Testing node version of oba-wrapper

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
+const API = require("oba-wrapper/node");
 const port = 1337;
+
+const api = new API({
+	key: "1e19898c87464e239192c8bfe422f280"
+});
+
+// (async()=>{
+// 	(await api.createStream("search/test{1}"))
+// 		.pipe(res => console.log("results:", res))
+// 		.catch(console.error);
+// })();
 
 app
 	.set("view engine", "ejs")
@@ -10,13 +21,11 @@ app
 	.get("/", (req, res) => {
 		res.render("home.ejs", {
 			filter: "home"
-		});
-	})
+		})})
 	.get("/:filter", (req, res) => {
 		res.render("home.ejs", {
 			filter: req.params.filter
-		});
-	})
+		})})
 	.post("/:filter", (req, res) => {
 		const items = [
 			{
@@ -39,8 +48,7 @@ app
 		res.render("search.ejs", {
 			filter: req.params.filter,
 			items
-		});
-	})
+		})});
 
 app.listen(port, () => {
 	console.log(`Running on port ${port}.`);
